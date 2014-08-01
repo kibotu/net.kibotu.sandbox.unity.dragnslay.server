02509cb7c36ee9676d5d7e50d647b54c2bc5bd4c
Jan Rabe <jan.rabe@wooga.net>
2014-08-01 15:10:24 +0200

    uploading layouts

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+process.env.NODE_ENV = 'development';
+
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -14,15 +17,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cookieParser());
+app.use(favicon(__dirname + '/public/img/favicon.png'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/bower_components'));
+app.locals.pretty = true;
+
+//var _ = require('underscore');
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/admin', admin);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET admin console */
 router.get('/', function(req, res) {
-  res.send('');
+    res.render('admin', { title: 'Admin Console' });
 });
 
 module.exports = router;
