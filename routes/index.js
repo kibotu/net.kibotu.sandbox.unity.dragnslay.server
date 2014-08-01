c500466568dd1c52643d9748202a5057d8ef0464
Jan Rabe <jan.rabe@wooga.net>
2014-08-01 09:08:18 +0200

    redirecting start page to static page for bots

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.writeHead(302, {
+        'Location': 'http://www.kibotu.net'
+    });
+    res.end();
 });
 
 module.exports = router;
