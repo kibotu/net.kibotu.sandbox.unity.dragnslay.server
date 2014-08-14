b484ddb0822d8f987832d313c5be4052a8611bf3
Jan Rabe <jan.rabe@wooga.net>
2014-08-14 18:36:04 +0200

    bower folder

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(favicon(__dirname + '/public/img/favicon.png'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/components'));
 app.locals.pretty = true;
 
 //var _ = require('underscore');
